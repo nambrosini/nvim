d0cdcedd1ace262d96fe0142e7ab1b03893daf9e
nambrosini <nico.ambrosini@icloud.com>
2024-06-09 11:58:33 +0200

    telescope and lspconfig now load on startup

--- lua/nambrosini/plugins/lsp/lspconfig.lua
+++ lua/nambrosini/plugins/lsp/lspconfig.lua
@@ -1,6 +1,5 @@
 return {
 	"neovim/nvim-lspconfig",
-	event = { "BufReadPre", "BufNewFile" },
 	dependencies = {
 		{ "williamboman/mason.nvim", config = true },
 		"williamboman/mason-lspconfig.nvim",
