1f776fa940e46bcc68572d11a56bcc33d053d3ef
nambrosini <nico.ambrosini@icloud.com>
2024-07-27 16:14:40 +0200

    Added stuff

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -3,64 +3,90 @@
 --
 -- See the kickstart.nvim README for more information
 return {
-  {
-    'theprimeagen/harpoon',
-    config = function()
-      local mark = require 'harpoon.mark'
-      local ui = require 'harpoon.ui'
+	{
+		"theprimeagen/harpoon",
+		config = function()
+			local mark = require("harpoon.mark")
+			local ui = require("harpoon.ui")
 
-      vim.keymap.set('n', '<leader>a', mark.add_file)
-      vim.keymap.set('n', '<C-e>', ui.toggle_quick_menu)
+			vim.keymap.set("n", "<leader>a", mark.add_file)
+			vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
 
-      vim.keymap.set('n', '<C-h>', function()
-        ui.nav_file(1)
-      end)
-      vim.keymap.set('n', '<C-t>', function()
-        ui.nav_file(2)
-      end)
-      vim.keymap.set('n', '<C-n>', function()
-        ui.nav_file(3)
-      end)
-      vim.keymap.set('n', '<C-s>', function()
-        ui.nav_file(4)
-      end)
-    end,
-  },
-  {
-    'tpope/vim-fugitive',
-    config = function()
-      vim.keymap.set('n', '<leader>gs', vim.cmd.Git)
-    end,
-  },
-  {
-    'mbbill/undotree',
-    config = function()
-      vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle)
-    end,
-  },
-  {
-    'folke/trouble.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-    opts = {},
-  },
-  {
-    'github/copilot.vim',
-  },
-  {
-    'windwp/nvim-autopairs',
-    event = 'InsertEnter',
-    config = true,
-  },
-  {
-    'rust-lang/rust.vim',
-    ft = 'rust',
-    init = function()
-      vim.g.rustfmt_autosave = 1
-    end,
-  },
-  {
-    'mrcjkb/rustaceanvim',
-    version = '^4', -- Recommended
-    lazy = false, -- This plugin is already lazy
-  },
+			vim.keymap.set("n", "<C-h>", function()
+				ui.nav_file(1)
+			end)
+			vim.keymap.set("n", "<C-t>", function()
+				ui.nav_file(2)
+			end)
+			vim.keymap.set("n", "<C-n>", function()
+				ui.nav_file(3)
+			end)
+			vim.keymap.set("n", "<C-s>", function()
+				ui.nav_file(4)
+			end)
+		end,
+	},
+	{
+		"tpope/vim-fugitive",
+		config = function()
+			vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+		end,
+	},
+	{
+		"mbbill/undotree",
+		config = function()
+			vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
+		end,
+	},
+	{
+		"folke/trouble.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		opts = {},
+	},
+	{
+		"github/copilot.vim",
+		event = "InsertEnter",
+	},
+	{
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		config = true,
+	},
+	{
+		"rust-lang/rust.vim",
+		event = { "BufRead", "BufNewFile" },
+		ft = "rust",
+		init = function()
+			vim.g.rustfmt_autosave = 1
+		end,
+	},
+	{
+		"mrcjkb/rustaceanvim",
+		version = "^4", -- Recommended
+		lazy = false, -- This plugin is already lazy
+	},
+	-- {
+	-- 	"towolf/vim-helm",
+	-- 	event = { "BufRead", "BufNewFile" },
+	-- 	ft = "helm",
+	-- },
+	{
+		"nvim-java/nvim-java",
+		ft = "java",
+		event = { "BufRead", "BufNewFile" },
+	},
+	{
+		"saecki/crates.nvim",
+		ft = "rust",
+		event = { "BufRead", "BufNewFile" },
+	},
+	{
+		"JavaHello/spring-boot.nvim",
+		event = { "BufRead", "BufNewFile" },
+		ft = "java",
+		dependencies = {
+			"nvim-java/nvim-java",
+			"ibhagwan/fzf-lua",
+		},
+	},
 }
